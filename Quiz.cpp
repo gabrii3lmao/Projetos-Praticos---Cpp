38f7b44372c408607fdffb692ae2fdae24fefed8
gabrii3l <gabrielmluz56@gmail.com>
2025-08-03 18:12:51 -0300

    Update Quiz.cpp
    formatei o código

--- Quiz.cpp
+++ Quiz.cpp
@@ -3,16 +3,14 @@
 #include <random>
 #include "Questoes.h"
 
-
+//falta fazer a desgraça do menu onde o usuário vai escolher a matéria. 
+//NÃO SE ESQUECER DE FAZER O LAÇO DE REPETIÇÃO INFINITO PRO MENU
 
 int main() {
-
 	setlocale(LC_ALL, "");
 	using namespace std;
 
-
-	start();
-
-	funcAleatorizadora();
+	start(); // cabeçalho 
+	funcAleatorizadora(); // onde uma Questão aleatória vai ser chamada 
 	return 0;
 }
