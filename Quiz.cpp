c10c67d8447bc3440b292a4254ec9f2cd7df99ec
gabrii3l <gabrielmluz56@gmail.com>
2025-08-05 21:54:39 -0300

    Melhorei as funções e consertei o sistema de loop usando funções bool

--- Quiz.cpp
+++ Quiz.cpp
@@ -1,29 +1,30 @@
-#include <iostream>
-#include <iomanip>
-#include <random>
-#include "Questoes.h"
+		#include <iostream>
+		#include <iomanip>
+		#include "Questoes.h"
 
 
-int main() {
+		int main() {
 
-	setlocale(LC_ALL, "");
-	using namespace std;
+			setlocale(LC_ALL, "");
+			using namespace std;
 	
-	start();
-
-	switch (materias())
-	{
-		case 1:
-			bool continuar;
-		do
-		{
-			system("cls"); startMat(); continuar = funcAleatorizadoraMat();
-		} while (continuar == 1); break;
-	case 2: std::cout << "Tem nada aqui não rapaz\n"; break;
-	case 3: std::cout << "Tem nada aqui também não, rapaz\n"; break;
-	default:
-		break;
-	}
-	
-	return 0;
-}
+			start();
+			switch (materias())
+			{
+				case 1:
+					bool continuar;
+				do
+				{
+					system("cls"); startMat(); continuar = funcAleatorizadoraMat();
+				} while (continuar == 1); break;
+				case 2: 
+					do
+					{
+						system("cls"); startQuim(); continuar = funcAleatorizadoraQuim();
+					} while (continuar == 1); break;
+			case 3: std::cout << "Tem nada aqui também não, rapaz\n"; break;
+			default:
+				break;
+			}
+			return 0;
+		}
